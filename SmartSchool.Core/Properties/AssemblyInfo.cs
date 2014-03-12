522d367bb928e1efbbc6cee7483ef2e02c7f1474
dylan.huang <dylan.huang@ischool.com.tw>
2014-03-12 11:33:54 +0800

    (1)移除BIN&OBJ資料夾(2)註解與刪除(匯出匯入缺曠/獎/懲)功能(含權限設定)

--- SmartSchool.Core/Properties/AssemblyInfo.cs
+++ SmartSchool.Core/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // 您可以指定所有的值，也可以依照以下的方式，使用 '*' 將修訂和組建編號
 // 指定為預設值:
-[assembly: AssemblyVersion("1.0.4.2")]
-[assembly: AssemblyFileVersion("1.0.4.2")]
+[assembly: AssemblyVersion("1.0.4.5")]
+[assembly: AssemblyFileVersion("1.0.4.5")]
